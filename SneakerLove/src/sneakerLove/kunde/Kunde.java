334b3e3e95e0114f21cf98b7a6caa227f96187a0
tobiaspeilert <tobiaspeilert@tobiass-mbp.fritz.box>
2018-02-23 20:32:15 +0100

    Klenigkeit

--- SneakerLove/src/sneakerLove/kunde/Kunde.java
+++ SneakerLove/src/sneakerLove/kunde/Kunde.java
@@ -2,49 +2,49 @@ package sneakerLove.kunde;
 
 public class Kunde {
 
-    private String vorname;
-    private String nachname;
-    private String benutzername;
-    private String passwort;
-    private String email;
+	private String vorname;
+	private String nachname;
+	private String benutzername;
+	private String passwort;
+	private String email;
+// TODO: ID_Kunde
+	private Adresse adresse;
 
-    private Adresse adresse;
+	public Kunde() {
 
-    public Kunde() {
+	}
 
-    }
+	public Kunde(String vorname, String nachname, String benutzername, String passwort, String email, Adresse adresse) {
+		this.vorname = vorname;
+		this.nachname = nachname;
+		this.benutzername = benutzername;
+		this.passwort = passwort;
+		this.email = email;
+		this.adresse = adresse;
+	}
 
-    public Kunde(String vorname, String nachname, String benutzername, String passwort, String email, Adresse adresse) {
-	this.vorname = vorname;
-	this.nachname = nachname;
-	this.benutzername = benutzername;
-	this.passwort = passwort;
-	this.email = email;
-	this.adresse = adresse;
-    }
+	public String getVorname() {
+		return vorname;
+	}
 
-    public String getVorname() {
-	return vorname;
-    }
+	public String getNachname() {
+		return nachname;
+	}
 
-    public String getNachname() {
-	return nachname;
-    }
+	public String getBenutzername() {
+		return benutzername;
+	}
 
-    public String getBenutzername() {
-	return benutzername;
-    }
+	public String getPasswort() {
+		return passwort;
+	}
 
-    public String getPasswort() {
-	return passwort;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public String getEmail() {
-	return email;
-    }
-
-    public Adresse getAdresse() {
-	return adresse;
-    }
+	public Adresse getAdresse() {
+		return adresse;
+	}
 
 }
