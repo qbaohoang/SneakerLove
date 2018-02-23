6e57d21fb1a5ab54ad8aea317215db26660634a3
Artem <s.artem@gmx.de>
2018-02-23 16:30:06 +0100

    Artem 1st

--- SneakerLove/src/de/sneakerLove/controller/Main.java
+++ SneakerLove/src/de/sneakerLove/controller/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	System.out.println("Goodbye World");
 	System.out.println("Test Tobi");
+	System.out.println("Geht doch");
     }
 
 }
